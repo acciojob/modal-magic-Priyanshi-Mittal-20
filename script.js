8d96fdb261cd317bfd7e133b72e7f6febfa0102a
Priyanshi-Mittal-20 <priyanshimzn05@gmail.com>
2026-07-13 13:27:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+let btn=document.querySelector(".open-modal")
+let removeBtn=document.querySelector(".remove-btn")
+let modalEle=document.querySelector(".modal")
+btn.addEventListener("click",show)
+removeBtn.addEventListener("click",hide)
+
+function hide()
+{
+	modalEle.style.display="none"
+}
+
+function show()
+{
+	modalEle.style.display="block"
+}
+
+modalEle.addEventListener("click", function (event) {
+    if (event.target === modalEle) {
+        hide();
+    }
+});
